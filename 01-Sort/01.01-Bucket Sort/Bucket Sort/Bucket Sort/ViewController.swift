8c32f92b5104caf0099eddb513fffd925990f708
Smartech <rain2540@foxmail.com>
2017-09-18 14:23:43 +0800

    feat (Bucket / ViewController) : add an example

--- 01-Sort/01.01-Bucket Sort/Bucket Sort/Bucket Sort/ViewController.swift
+++ 01-Sort/01.01-Bucket Sort/Bucket Sort/Bucket Sort/ViewController.swift
@@ -25,6 +25,23 @@ class ViewController: UIViewController {
         let nu = [0, -1, -2, 3, 7, 6, 5, 8, 4, 1, 3, -5]
         let res = BucketSorter.sort(nums: nu)
         print("result: \n", res)
+
+        var numbers: [Int] = []
+        let start1 = Date(timeIntervalSinceNow: 0)
+        for _ in 0 ..< 1000 {
+            let num = arc4random() % 1_0000 + 1
+            numbers.append(Int(num))
+        }
+        let end1 = Date(timeIntervalSinceNow: 0)
+        let d1 = end1.timeIntervalSince1970 - start1.timeIntervalSince1970
+        print(d1)
+
+        let start3 = Date(timeIntervalSinceNow: 0)
+        let r = BucketSorter.sort(nums: numbers)
+        let end3 = Date(timeIntervalSinceNow: 0)
+        let d3 = end3.timeIntervalSince1970 - start3.timeIntervalSince1970
+        print(d3)
+        print(r)
     }
 
     //  MARK: IBActions
