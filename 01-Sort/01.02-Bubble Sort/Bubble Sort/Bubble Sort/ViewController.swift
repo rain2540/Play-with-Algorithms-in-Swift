0ab82725b6102fc11d2cd6cfcfb868c15f24b448
Smartech <rain2540@foxmail.com>
2017-09-18 14:24:33 +0800

    feat (Bubble Sort / ViewController) : add method and example

--- 01-Sort/01.02-Bubble Sort/Bubble Sort/Bubble Sort/ViewController.swift
+++ 01-Sort/01.02-Bubble Sort/Bubble Sort/Bubble Sort/ViewController.swift
@@ -13,6 +13,36 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        var nums: [Int] = []
+        let start1 = Date(timeIntervalSinceNow: 0)
+        for _ in 0 ..< 500 {
+            let num = arc4random() % 1_0000 + 1
+            nums.append(Int(num))
+        }
+        let end1 = Date(timeIntervalSinceNow: 0)
+        let d1 = end1.timeIntervalSince1970 - start1.timeIntervalSince1970
+        print(d1)
+        
+        let start2 = Date(timeIntervalSinceNow: 0)
+        _ = sort(nums)
+        let end2 = Date(timeIntervalSinceNow: 0)
+        let d2 = end2.timeIntervalSince1970 - start2.timeIntervalSince1970
+        print(d2)
+    }
+
+    func sort(_ nums: [Int]) -> [Int] {
+        var result = nums
+        for i in 1 ..< result.count {
+            for j in 1 ... result.count - i {
+                if result[j] < result [j - 1] {
+                    result.swapAt(j, j - 1)
+                    print(i, j, result)
+                }
+            }
+            print()
+        }
+        return result
     }
 
     override func didReceiveMemoryWarning() {
