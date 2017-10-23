d3f14371be093ffb1562a26629785663abeda71f
Smartech <rain2540@foxmail.com>
2017-10-23 14:26:13 +0800

    feat (Quick Sort) : add methods

--- 01-Sort/01.03-Quick Sort/Quick Sort/Quick Sort/ViewController.swift
+++ 01-Sort/01.03-Quick Sort/Quick Sort/Quick Sort/ViewController.swift
@@ -12,14 +12,64 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        /*var numbers: [Int] = []
+        let start1 = Date(timeIntervalSinceNow: 0)
+        for _ in 0 ..< 1000 {
+            let num = arc4random() % 1_0000 + 1
+            numbers.append(Int(num))
+        }
+        let end1 = Date(timeIntervalSinceNow: 0)
+        let d1 = end1.timeIntervalSince1970 - start1.timeIntervalSince1970
+        print(d1)
+
+        let start3 = Date(timeIntervalSinceNow: 0)
+        quicksort(nums: &numbers)
+        let end3 = Date(timeIntervalSinceNow: 0)
+        let d3 = end3.timeIntervalSince1970 - start3.timeIntervalSince1970
+        print(d3)*/
+
+        var nums = [7, 3, 5, 2, 6, 1, 4, 0]
+        quicksort(nums: &nums)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    func quicksort(nums: inout [Int]) {
+        let left = 0, right = nums.count - 1
+        func quicksort(nums: inout [Int], left: Int, right: Int) {
+            var i, j, temp: Int
+            if left > right {
+                return
+            }
+            temp = nums[left]    // temp 中存储基准数
+            i = left
+            j = right
+            while i != j {
+                // 顺序很重要, 要先从右往左找
+                while nums[j] >= temp && i < j {
+                    j -= 1
+                }
+                // 再从左往右找
+                while nums[i] <= temp && i < j {
+                    i += 1
+                }
+                // 交换两个数在数组中的位置
+                if i < j {    // 当 i 和 j 没有相遇时
+                    nums.swapAt(i, j)
+                }
+                print(i, j, nums)
+            }
+            // 最终将基准数归位
+            nums[left] = nums[i]
+            nums[i] = temp
 
+            quicksort(nums: &nums, left: left, right: i - 1)    // 继续处理左边的, 这是一个递归的过程
+            quicksort(nums: &nums, left: i + 1, right: right)    // 继续处理左边的, 这是一个递归的过程
+            print(i, j, nums)
+        }
+        quicksort(nums: &nums, left: left, right: right)
+    }
 }
-
