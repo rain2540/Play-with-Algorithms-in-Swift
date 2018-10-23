46417df8c52deb120254f1bc4aa77f61d150f214
Smartech <rain2540@foxmail.com>
2018-10-23 08:19:37 +0800

    feat (Leet Code) : add content

--- LeetCode/01-Array.playground/Pages/Remove Duplicates 2.xcplaygroundpage/Contents.swift
+++ LeetCode/01-Array.playground/Pages/Remove Duplicates 2.xcplaygroundpage/Contents.swift
@@ -10,6 +10,32 @@ import Foundation
 //:
 //: Your function should return length = 5, and A is now [1,1,2,2,3].
 
+func removeDuplicates(array: inout [Int]) -> Int {
+    if array.count == 0 {
+        return 0
+    }
 
+    var j = 0
+    var num = 0
+    for i in 1 ..< array.count {
+        if array[j] == array[i] {
+            num += 1
+            if num < 2 {
+                j += 1
+                array[j] = array[i]
+            }
+        } else {
+            j += 1
+            array[j] = array[i]
+            num = 0
+        }
+    }
+    return j + 1
+}
+
+var array = [1, 1, 1, 2, 2, 3]
+print(array)
+removeDuplicates(array: &array)
+print(array)
 
 //: [Next](@next)
