990058ea8d253519a6e6c9fd98fa96d4e8cc36ab
Smartech <rain2540@foxmail.com>
2018-11-30 15:27:55 +0800

    feat (Leet Code) : add a problem

--- LeetCode/01-Array.playground/Pages/Pascal Triangle 2.xcplaygroundpage/Contents.swift
+++ LeetCode/01-Array.playground/Pages/Pascal Triangle 2.xcplaygroundpage/Contents.swift
@@ -2,6 +2,8 @@
 
 import Foundation
 
-var str = "Hello, playground"
+//: Given an index k, return the kth row of the Pascal's triangle.
+//:
+//: For example, given k = 3, Return [1,3,3,1].
 
 //: [Next](@next)
