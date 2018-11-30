b7705915f7c62a95b82e1d3afa605c664ab080d9
Smartech <rain2540@foxmail.com>
2018-11-30 10:16:56 +0800

    docs (Leet Code) : comment in chinese

--- LeetCode/01-Array.playground/Pages/Remove Duplicates.xcplaygroundpage/Contents.swift
+++ LeetCode/01-Array.playground/Pages/Remove Duplicates.xcplaygroundpage/Contents.swift
@@ -4,13 +4,23 @@ import Foundation
 
 //: ## Remove Duplicates from Sorted Array
 //:
-//: Given a sorted array, remove the duplicates in place such that > each element appear only once and return the new length.
+//: Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
 //:
 //: Do not allocate extra space for another array, you must do this in place with constant memory.
 //:
 //: For example, Given input array A = [1,1,2],
 //:
 //: Your function should return length = 2, and A is now [1,2].
+//:
+//: ## 从已排序的数组中删除重复项
+//:
+//: 给定排序数组，删除重复项，使每个元素只出现一次并返回新长度。
+//:
+//: 不要为另一个数组分配额外的空间，你必须使用常量内存来做到这一点。
+//:
+//: 例如，给定输入数组 A = [1,1,2]，
+//:
+//: 你的函数应该返回 length = 2，并且 A 现在是[1,2]。
 
 func removeDuplicates(in array: inout [Int]) -> Int {
     if array.count == 0 {
