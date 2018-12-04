b813dd9cd01d1402dd05c74360d47af2b79756d1
Smartech <rain2540@foxmail.com>
2018-12-04 11:24:08 +0800

    feat (Leet Code) : add implementation

--- LeetCode/01-Array.playground/Pages/Pascal Triangle.xcplaygroundpage/Contents.swift
+++ LeetCode/01-Array.playground/Pages/Pascal Triangle.xcplaygroundpage/Contents.swift
@@ -32,4 +32,24 @@ import Foundation
 //:
 //: * 对于第 k (k > 2) 层第 n (n > 1 && n < k) 个元素 A[k][n], A[k][n] = A[k - 1][n - 1] + A[k - 1][n]
 
+func generate(numRows: Int) -> Array<[Int]> {
+    var vals = Array<[Int]>(repeating: [], count: numRows)
+
+    for i in 0 ..< numRows {
+        vals[i] = [Int](repeating: 0, count: i + 1)
+        vals[i][0] = 1
+        vals[i][vals[i].count - 1] = 1
+
+        if i > 0 {
+            for j in 1 ..< vals[i].count - 1 {
+                vals[i][j] = vals[i - 1][j - 1] + vals[i - 1][j]
+            }
+        }
+    }
+
+    return vals
+}
+
+print(generate(numRows: 5))
+
 //: [Next](@next)
