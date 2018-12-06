64e2fa301aef70e80e2dad9d0b1f19ef756488b8
Smartech <rain2540@foxmail.com>
2018-12-06 08:35:22 +0800

    style (Leet Code) : update style

--- LeetCode/01-Array.playground/Pages/Pascal Triangle 2.xcplaygroundpage/Contents.swift
+++ LeetCode/01-Array.playground/Pages/Pascal Triangle 2.xcplaygroundpage/Contents.swift
@@ -6,7 +6,7 @@ import Foundation
 //: Given an index k, return the kth row of the Pascal's triangle.
 //:
 //: For example, given k = 3, Return [1,3,3,1].
-//:
+
 //: ## 帕斯卡三角 II
 //: 给定索引 k，返回 Pascal 三角形的第 k 行。
 //:
