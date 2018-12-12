417a9363f220b27d328fb85c8aaab2a53bca86ca
Smartech <rain2540@foxmail.com>
2018-12-12 10:27:34 +0800

    feat (Leet Code) : another implementation for "Merge Sorted Array"

--- LeetCode/01-Array.playground/Pages/Merge Sorted Array.xcplaygroundpage/Contents.swift
+++ LeetCode/01-Array.playground/Pages/Merge Sorted Array.xcplaygroundpage/Contents.swift
@@ -8,7 +8,41 @@ import Foundation
 //:
 //: Note: You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. The number of elements initialized in A and B are m and n respectively.
 
-func merge(a: [Int], b: [Int]) -> [Int] {
+func merge(b: [Int], into a: inout [Int]) {
+    var i = a.count + b.count - 1
+    var j = a.count - 1
+    var k = b.count - 1
+
+    a.append(contentsOf: b)
+
+    while i >= 0 {
+        if j >= 0 && k >= 0 {
+            if a[j] > b[k] {
+                a[i] = a[j]
+                j -= 1
+            } else {
+                a[i] = b[k]
+                k -= 1
+            }
+        } else if j >= 0 {
+            a[i] = a[j]
+            j -= 1
+        } else if k >= 0 {
+            a[i] = b[k]
+            k -= 1
+        }
+        i -= 1
+    }
+}
+
+var a = [1, 3, 5]
+var b = [2, 4, 6]
+merge(b: b, into: &a)
+
+
+//: Another implementation (with return value)
+
+func merged(a: [Int], b: [Int]) -> [Int] {
     var i = a.count + b.count - 1
     var j = a.count - 1
     var k = b.count - 1
@@ -36,9 +70,6 @@ func merge(a: [Int], b: [Int]) -> [Int] {
     return res
 }
 
-var a = [1, 3, 5]
-var b = [2, 4, 6]
-print(merge(a: a, b: b))
-
+print(merged(a: a, b: b))
 
 //: [Next](@next)
