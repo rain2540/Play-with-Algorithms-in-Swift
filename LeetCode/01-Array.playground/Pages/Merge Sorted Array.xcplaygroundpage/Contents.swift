3e640f1e5cf494f9ed419910513c924b6e24642b
Smartech <rain2540@foxmail.com>
2018-12-12 09:59:48 +0800

    feat (Leet Code) : add implementation

--- LeetCode/01-Array.playground/Pages/Merge Sorted Array.xcplaygroundpage/Contents.swift
+++ LeetCode/01-Array.playground/Pages/Merge Sorted Array.xcplaygroundpage/Contents.swift
@@ -8,6 +8,37 @@ import Foundation
 //:
 //: Note: You may assume that A has enough space (size that is greater or equal to m + n) to hold additional elements from B. The number of elements initialized in A and B are m and n respectively.
 
+func merge(a: [Int], b: [Int]) -> [Int] {
+    var i = a.count + b.count - 1
+    var j = a.count - 1
+    var k = b.count - 1
+
+    var res = Array<Int>(repeating: 0, count: a.count + b.count)
+
+    while i >= 0 {
+        if j >= 0 && k >= 0 {
+            if a[j] > b[k] {
+                res[i] = a[j]
+                j -= 1
+            } else {
+                res[i] = b[k]
+                k -= 1
+            }
+        } else if j >= 0 {
+            res[i] = a[j]
+            j -= 1
+        } else if k >= 0 {
+            res[i] = b[k]
+            k -= 1
+        }
+        i -= 1
+    }
+    return res
+}
+
+var a = [1, 3, 5]
+var b = [2, 4, 6]
+print(merge(a: a, b: b))
 
 
 //: [Next](@next)
