8356f26f04d7728b311786c1856b6eb13b4cde00
rain2540 <rain2540@foxmail.com>
2020-07-31 03:25:14 +0800

    refactor (Q099 - Contents) : update examples

--- Swift-100-Questions/Question99.playground/Contents.swift
+++ Swift-100-Questions/Question99.playground/Contents.swift
@@ -18,7 +18,7 @@ func gcd(a: Int, b: Int) -> Int {
 }
 
 let x = 75, y = 100
-let result = gcd(a: 75, b: 100)
+let result = gcd(a: x, b: y)
 print("\(x) 和 \(y) 的最大公约数是 \(result)")
 
 
@@ -34,7 +34,8 @@ func gcdLoop(a: Int, b: Int) -> Int {
     return y
 }
 
-gcdLoop(a: 75, b: 100)
+let result = gcdLoop(a: x, b: y)
+print("\(x) 和 \(y) 的最大公约数是 \(result)")
 
 
 //  递归
@@ -42,7 +43,8 @@ func gcdRecursive(a: Int, b: Int) -> Int {
     return b == 0 ? a : gcdRecursive(a: b, b: a % b)
 }
 
-gcdRecursive(a: 75, b: 100)
+let result = gcdRecursive(a: x, b: y)
+print("\(x) 和 \(y) 的最大公约数是 \(result)")
 
 
 //: 附：求两个数的最小公倍数 (Least Common Multiple)
