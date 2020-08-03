5edacbd35858baecc407fdd5230ac5bf6abe880a
Smartech <rain2540@foxmail.com>
2020-08-03 14:20:38 +0800

    refactor (Q99) : update variable name

--- Swift-100-Questions/Question99.playground/Contents.swift
+++ Swift-100-Questions/Question99.playground/Contents.swift
@@ -18,8 +18,8 @@ func gcd(a: Int, b: Int) -> Int {
 }
 
 let x = 75, y = 100
-let result = gcd(a: x, b: y)
-print("\(x) 和 \(y) 的最大公约数是 \(result)")
+let result1 = gcd(a: x, b: y)
+print("\(x) 和 \(y) 的最大公约数是 \(result1)")
 
 
 //  循环
@@ -34,8 +34,8 @@ func gcdLoop(a: Int, b: Int) -> Int {
     return y
 }
 
-let result = gcdLoop(a: x, b: y)
-print("\(x) 和 \(y) 的最大公约数是 \(result)")
+let result2 = gcdLoop(a: x, b: y)
+print("\(x) 和 \(y) 的最大公约数是 \(result2)")
 
 
 //  递归
@@ -43,8 +43,8 @@ func gcdRecursive(a: Int, b: Int) -> Int {
     return b == 0 ? a : gcdRecursive(a: b, b: a % b)
 }
 
-let result = gcdRecursive(a: x, b: y)
-print("\(x) 和 \(y) 的最大公约数是 \(result)")
+let result3 = gcdRecursive(a: x, b: y)
+print("\(x) 和 \(y) 的最大公约数是 \(result3)")
 
 
 //: 附：求两个数的最小公倍数 (Least Common Multiple)
